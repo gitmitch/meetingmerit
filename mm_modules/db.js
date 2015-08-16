7b5515c582f042817a92c475cfef8ea82eac1353
gitmitch <mitch.huff@gmail.com>
2015-08-16 10:45:25 -0700

    updating environment variables to match openshift format

--- mm_modules/db.js
+++ mm_modules/db.js
@@ -3,11 +3,11 @@ var mysql = require('mysql')
 
 function runQuery() {
     var connection = mysql.createConnection({
-        host     : process.env.RDS_HOSTNAME,
-        user     : process.env.RDS_USERNAME,
-        password : process.env.RDS_PASSWORD,
-        port 	 : process.env.RDS_PORT,
-        database : process.env.DATABASE_NAME
+        host     : process.env.OPENSHIFT_MYSQL_DB_HOST,
+        user     : process.env.MM_DB_USERNAME,
+        password : process.env.MM_DB_PASSWORD,
+        port 	 : process.env.OPENSHIFT_MYSQL_DB_PORT,
+        database : process.env.MM_DB_NAME
     })
     connection.query.apply(connection, arguments)
     connection.end()
